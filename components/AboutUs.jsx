39c160fa04cf904f4349d9b21ad4dac2f7a602ad
Jelle Rienstra <jrienstra86@gmail.com>
2021-11-05 22:05:38 +0100

    wie zijn wij

--- components/AboutUs.jsx
+++ components/AboutUs.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Link from 'next/link';
+import Card from './Card';
+import team from '../data/team';
 
 const MaxWidth = styled.div`
-    max-width: 1024px;
-    margin: 0 auto;
-    padding: 32px 16px;
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 32px 16px;
 
-    @media(min-width: 1024px) {
-      padding: 72px 24px;
-    }
+  @media(min-width: 1024px) {
+    padding: 72px 24px;
+  }
 `;
 
 const Title = styled(Typography)`
-    text-align: center;
-    margin-bottom: 32px;
+  text-align: center;
+  margin-bottom: 32px;
+`;
+
+const Description = styled(Typography)`
+  margin: 0 auto 72px;
+  text-align: center;
 `;
 
 const Background = styled.div`
@@ -25,6 +34,29 @@ const AboutUs = () => (
   <Background>
     <MaxWidth>
       <Title variant="h4" color="primary">Wie zijn wij?</Title>
+      <Description variant="body1" color="primary">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consequat.</Description>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={5}
+      >
+        {team.map((person) => {
+          const {
+            name, description, image,
+          } = person;
+          return (
+            <Grid item xs={12} sm={12} md={4}>
+              <Card
+                title={name}
+                description={description}
+                image={image}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
     </MaxWidth>
   </Background>
 );
---
+++ data/team.js
@@ -0,0 +1,16 @@
+import anton from '../public/anton.jpeg';
+
+const team = [
+  {
+    name: 'Anton',
+    image: anton,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate luctus odio at pretium. Nunc facilisis enim libero, eu malesuada turpis feugiat non.',
+  },
+  {
+    name: 'Fer',
+    image: anton,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate luctus odio at pretium. Nunc facilisis enim libero, eu malesuada turpis feugiat non.',
+  },
+];
+
+export default team;
